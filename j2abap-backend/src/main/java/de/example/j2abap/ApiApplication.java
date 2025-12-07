07b341bfed889eed6428a5bc0e0308b342079563
Steven Illg <bztyla@hotmail.de>
2025-12-07 16:39:04 +0100

    Create ApiApplication.java

---
+++ j2abap-backend/src/main/java/de/example/j2abap/ApiApplication.java
@@ -0,0 +1,11 @@
+package de.example.j2abap.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ApiApplication.class, args);
+  }
+}
