7a93479019acfe3c53b11ecf5737dad0123b111e
Steven Illg <bztyla@hotmail.de>
2025-12-07 18:28:59 +0100

    Update HealthController.java

--- j2abap-backend/src/main/java/de/example/j2abap/api/HealthController.java
+++ j2abap-backend/src/main/java/de/example/j2abap/api/HealthController.java
@@ -3,6 +3,7 @@ package de.example.j2abap.api;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping("/api")
@@ -12,3 +13,4 @@ public class HealthController {
     return ResponseEntity.ok("ok"); 
   }
 }
+
