24077dc64bed73a4d71fc60471623dcd2432058d
Steven Illg <bztyla@hotmail.de>
2025-12-07 16:33:20 +0100

    Update TranslateController.java

--- j2abap-backend/src/main/java/de/example/j2abap/api/TranslateController.java
+++ j2abap-backend/src/main/java/de/example/j2abap/api/TranslateController.java
@@ -1,5 +1,6 @@
 package de.example.j2abap.api;
 
+import de.example.j2abap.JavaToAbapTranslator;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
