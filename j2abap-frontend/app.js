d7b96a9074200208ae0d57901c949320c8060925
Steven Illg <bztyla@hotmail.de>
2025-12-07 17:33:19 +0100

    Update app.js

--- j2abap-frontend/app.js
+++ j2abap-frontend/app.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://scattered-lebbie-steven-illg-it-f8b8abdf.koyeb.app";
+const API_BASE = "scattered-lebbie-steven-illg-it-f8b8abdf.koyeb.app/";
 
 const $in = document.getElementById("in");
 const $out = document.getElementById("out");
