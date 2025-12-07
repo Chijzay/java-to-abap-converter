e340523688af2f80d65feca66f77508ed3ecb1da
Steven Illg <bztyla@hotmail.de>
2025-12-07 18:50:42 +0100

    Update HealthController.java

--- j2abap-backend/src/main/java/de/example/j2abap/api/HealthController.java
+++ j2abap-backend/src/main/java/de/example/j2abap/api/HealthController.java
@@ -3,14 +3,12 @@ package de.example.j2abap.api;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
 public class HealthController {
 
-  @GetMapping(path = {"/health", "/health/"}, produces = MediaType.TEXT_PLAIN_VALUE)
+  @GetMapping(path = {"/api/health", "/api/health/"}, produces = MediaType.TEXT_PLAIN_VALUE)
   public ResponseEntity<String> health() {
     return ResponseEntity.ok("ok");
   }
