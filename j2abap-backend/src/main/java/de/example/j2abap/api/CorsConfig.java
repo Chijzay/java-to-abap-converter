eb5e44e260996b706dfbaefe7129f07c9ba92da9
Steven Illg <bztyla@hotmail.de>
2025-12-07 14:19:43 +0100

    Update CorsConfig.java

--- j2abap-backend/src/main/java/de/example/j2abap/api/CorsConfig.java
+++ j2abap-backend/src/main/java/de/example/j2abap/api/CorsConfig.java
@@ -1,29 +1,16 @@
 package de.example.j2abap.api;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-
-  @Bean
-  public WebMvcConfigurer corsConfigurer() {
-    return new WebMvcConfigurer() {
-      @Override
-      public void addCorsMappings(CorsRegistry registry) {
-          registry.addMapping("/api/**")
-          .allowedOriginPatterns(
-          "http://localhost:*",
-          "http://127.0.0.1:*",
-          "https://*.github.io",
-          "https://*.koyeb.app"
-          )
-          .allowedMethods("GET", "POST", "OPTIONS")
-          .allowedHeaders("*");
-
-      }
-    };
-  }
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "OPTIONS")
+                .allowedHeaders("*");
+    }
 }
