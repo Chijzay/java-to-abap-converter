87b2018e0aa43da75311e3ccf979b1461f81f783
Steven Illg <bztyla@hotmail.de>
2025-12-07 14:39:36 +0100

    Update app.js

--- j2abap-frontend/app.js
+++ j2abap-frontend/app.js
@@ -1,6 +1,4 @@
-const API_BASE = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
-  ? "http://localhost:8080"
-  : "https://scattered-lebbie-steven-illg-it-f8b8abdf.koyeb.app";
+const API_BASE = "https://scattered-lebbie-steven-illg-it-f8b8abdf.koyeb.app";
 
 const $in = document.getElementById("in");
 const $out = document.getElementById("out");
